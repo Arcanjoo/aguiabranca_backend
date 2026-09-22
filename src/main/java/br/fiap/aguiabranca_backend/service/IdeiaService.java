f63b93b5ff58acddd153fbc42c97e4f53f9a8c9b
Arcanjoo <joaopdr924@gmail.com>
2026-09-21 22:52:46 -0300

    feat: adiciona chave do gemini diretamente no IdeiaService

--- src/main/java/br/fiap/aguiabranca_backend/service/IdeiaService.java
+++ src/main/java/br/fiap/aguiabranca_backend/service/IdeiaService.java
@@ -32,7 +32,7 @@ public class IdeiaService {
         try {
             // Chamada à API do Google Gemini
             RestTemplate restTemplate = new RestTemplate();
-            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + geminiApiKey;
+            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + geminiApiKey;
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
